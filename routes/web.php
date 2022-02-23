2422d9610f92641c0fb8646d01e6cb3ee322446d
vennymeida <vnymeida@gmail.com>
2022-02-23 14:01:13 +0700

    Praktikum Pertama

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+    //return view('welcome');
+//});
+
+//PRAKTIKUM 1
+//nomor1
 Route::get('/', function () {
-    return view('welcome');
+    echo "Hi! Selamat Datang di Website Laravel";}) -> name('hello'); 
+//nomor2 
+Route::get('/about', function () {
+    echo "2041720100 <br>";
+    echo "Venny Meida Hersianty <br>";
+    echo "TI-2G";
+}) -> name('about');
+//nomor3 
+Route::get('/articles/{id}', function ($id) {
+    echo "Ini adalah halaman Artikel dengan ID: ".$id;
 });
