1f3901831bd21e4c9165f3a9558074adc9762631
vennymeida <vnymeida@gmail.com>
2022-02-23 15:15:08 +0700

    Praktikum Kedua

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        return "Hi! Selamat Datang di Website Laravel";
+    }
+    
+    public function about(){
+        return "2041720233 <br> Thirsya Widya Sulaiman <br> TI-2G";
+    }
+    
+    public function articles ($id){
+        return "Ini adalah halaman Artikel dengan ID: $id";
+    }
+}
